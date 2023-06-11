680e7df033d41f0dfaab6c1c1b525413139f915b
Steven <github@steven-h.net>
2023-06-11 01:45:11 -0400

    Start focus testing rendering

--- scripts/lola/event/Render.lua
+++ scripts/lola/event/Render.lua
@@ -2,10 +2,12 @@ local Color         = require "system.utils.Color"
 local CurrentLevel  = require "necro.game.level.CurrentLevel"
 local Entities      = require "system.game.Entities"
 local Event         = require "necro.event.Event"
+local Focus         = require "necro.game.character.Focus"
 local OutlineFilter = require "necro.render.filter.OutlineFilter"
 local Player        = require "necro.game.character.Player"
 local PlayerList    = require "necro.client.PlayerList"
 local Render        = require "necro.render.Render"
+local Utilities     = require "system.utils.Utilities"
 
 local BLUE   = Color.rgb(43, 66, 180)
 local GREEN  = Color.rgb(66, 180, 43)
@@ -16,9 +18,30 @@ Event.render.add("outlineClaimedItems", { order = "outlines", sequence = 1 },
   function(ev)
     if CurrentLevel.isLobby() then return end
 
-    local pid = PlayerList.getLocalPlayerID()
-    local plr = Player.getPlayerEntity(pid)
+    local focus = Focus.getAll()
 
+    -- Get focused players and their lowPercentAllowedItems
+    local pids = {}
+    local itms = {}
+    local blue = false
+
+    for i, p in ipairs(focus) do
+      if p.NixsChars_forcedLowPercent and p.NixsChars_forcedLowPercent.active then
+        blue = true
+
+        if p.lowPercent then
+          for k in pairs(p.lowPercent.allowedItems) do
+            itms[k] = true
+          end
+        end
+      end
+
+      if p.NixsChars_descentCollectItems then
+        pids[p.controllable.playerID] = true
+      end
+    end
+
+    -- Iterate all items
     for item in Entities.entitiesWithComponents { "item" } do
       local color
 
@@ -27,27 +50,23 @@ Event.render.add("outlineClaimedItems", { order = "outlines", sequence = 1 },
         goto continue
       end
 
-      if plr.lowPercent then
-        if plr.lowPercent.allowedItems[item.name]
-            and plr.NixsChars_forcedLowPercent then
+      if blue and itms[item.name] then
+        color = BLUE -- blue
+      elseif item.NixsChars_revealedBy then
+        if not (item.itemNegateLowPercent and item.itemNegateLowPercent.active) then
           color = BLUE -- blue
-        elseif item.NixsChars_revealedBy then
-          if not (item.itemNegateLowPercent and item.itemNegateLowPercent.active) then
-            color = BLUE -- blue
-          elseif item.NixsChars_revealedBy.playerID == pid
-              and plr.NixsChars_descentCollectItems then
-            if item.item.singleChoice == 0 then
-              color = GREEN -- green
-            else
-              color = YELLOW -- yellow
-            end
-          elseif item.NixsChars_revealedBy.playerID ~= 0
-              and Player.getPlayerEntity(item.NixsChars_revealedBy.playerID).NixsChars_descentCollectItems then
-            color = SILVER -- silver
+        elseif pids[item.NixsChars_revealedBy.playerID] then
+          if item.item.singleChoice == 0 then
+            color = GREEN -- green
+          else
+            color = YELLOW -- yellow
           end
-        elseif not item.itemCurrency then
-          color = BLUE -- blue
+        elseif item.NixsChars_revealedBy.playerID ~= 0
+            and Player.getPlayerEntity(item.NixsChars_revealedBy.playerID).NixsChars_descentCollectItems then
+          color = SILVER -- silver
         end
+      elseif not item.itemCurrency then
+        color = BLUE -- blue
       end
 
       if color ~= nil then
